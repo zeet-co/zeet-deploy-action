8ebbd41738a68de0750aac80c1c4eecaba47da3d
hi019 <65871571+hi019@users.noreply.github.com>
2022-05-24 19:38:48 -0400

    Update

--- index.ts
+++ index.ts
@@ -1,13 +1,20 @@
 import * as exec from '@actions/exec'
 import * as core from '@actions/core'
+import {installBinary} from 'action-utils'
 
 async function main(): Promise<void> {
-  await exec.exec('zeet', [
-    core.getInput('project'),
-    core.getInput('image') && `--image=${core.getInput('image')}`,
-    core.getInput('branch') && `--branch=${core.getInput('branch')}`,
-    core.getInput('follow') && `--follow=${core.getInput('wait')}`
-  ])
+  try {
+    await installBinary()
+
+    await exec.exec('zeet', [
+      core.getInput('project'),
+      core.getInput('image') && `--image=${core.getInput('image')}`,
+      core.getInput('branch') && `--branch=${core.getInput('branch')}`,
+      core.getInput('follow') && `--follow=${core.getInput('wait')}`
+    ])
+  } catch (e: unknown) {
+    if (e instanceof Error) core.error(e)
+  }
 }
 
 main()
