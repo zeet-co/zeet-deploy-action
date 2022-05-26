0403296fd394381400ecf4b1154fae8ba04a4509
hi019 <65871571+hi019@users.noreply.github.com>
2022-05-26 18:18:10 -0400

    Update

--- index.ts
+++ index.ts
@@ -14,12 +14,17 @@ async function main(): Promise<void> {
       core.getInput('follow') && `--follow=${core.getInput('wait')}`
     ]
 
-    const out = await exec.getExecOutput('zeet deploy', [
+    await exec.exec('zeet deploy', [
       core.getInput('project'),
       ...args.filter(a => a)
     ])
 
-    const links = out.stdout.match('(https?:\\/\\/zeet\\.co\\/repo[^\\s]+)')
+    const status = await exec.getExecOutput(
+      'zeet status',
+      [core.getInput('project')],
+      {silent: true}
+    )
+    const links = status.stdout.match('(https?:\\/\\/zeet\\.co\\/repo[^\\s]+)')
     core.setOutput('link', links ? links[0] : 'Not Found')
   } catch (e: unknown) {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
