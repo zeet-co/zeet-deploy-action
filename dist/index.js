1faaed6232de26c29a492e1f63a5ade6a736f7ca
hi019 <65871571+hi019@users.noreply.github.com>
2022-05-26 19:50:31 -0400

    Update

--- dist/index.js
+++ dist/index.js
@@ -28,34 +28,39 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const exec = __importStar(require("@actions/exec"));
 const core = __importStar(require("@actions/core"));
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const args = [
-                core.getInput('image') && `--image=${core.getInput('image')}`,
-                core.getInput('branch') && `--branch=${core.getInput('branch')}`,
-                core.getInput('follow') && `--follow=${core.getBooleanInput('wait')}`
-            ];
-            yield exec.exec('zeet deploy', [
-                core.getInput('project'),
-                ...args.filter(a => a)
-            ]);
-            core.setOutput('link', 'test');
-            // const status = await exec.getExecOutput(
-            //   'zeet status',
-            //   [core.getInput('project')],
-            //   {silent: true}
-            // )
-            // const links = status.stdout.match('(https?:\\/\\/zeet\\.co\\/repo[^\\s]+)')
-            // core.setOutput('link', links ? links[0] : 'Not Found')
-        }
-        catch (e) {
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            core.setFailed(e);
-        }
+        core.info(`deploy key: ${core.getInput('deploy_key')}`);
+        // core.debug(`deploy key: ${core.getInput('deploy_key')}`)
+        // core.debug(`wait: ${core.getBooleanInput('wait')}`)
+        //
+        // try {
+        //   const args = [
+        //     core.getInput('image') && `--image=${core.getInput('image')}`,
+        //     core.getInput('branch') && `--branch=${core.getInput('branch')}`,
+        //     core.getInput('follow') && `--follow=${core.getBooleanInput('wait')}`
+        //   ]
+        //
+        //   await exec.exec('zeet deploy', [
+        //     core.getInput('project'),
+        //     ...args.filter(a => a)
+        //   ])
+        //
+        //   core.setOutput('link', 'test')
+        //
+        //   // const status = await exec.getExecOutput(
+        //   //   'zeet status',
+        //   //   [core.getInput('project')],
+        //   //   {silent: true}
+        //   // )
+        //   // const links = status.stdout.match('(https?:\\/\\/zeet\\.co\\/repo[^\\s]+)')
+        //   // core.setOutput('link', links ? links[0] : 'Not Found')
+        // } catch (e: unknown) {
+        //   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        //   // @ts-ignore
+        //   core.setFailed(e)
+        // }
     });
 }
 main();
--- index.ts
+++ index.ts
@@ -1,36 +1,36 @@
-import * as exec from '@actions/exec'
 import * as core from '@actions/core'
 
 async function main(): Promise<void> {
-  core.debug(`deploy key: ${core.getInput('deploy_key')}`)
-  core.debug(`wait: ${core.getInput('wait')}`)
-
-  try {
-    const args = [
-      core.getInput('image') && `--image=${core.getInput('image')}`,
-      core.getInput('branch') && `--branch=${core.getInput('branch')}`,
-      core.getInput('follow') && `--follow=${core.getBooleanInput('wait')}`
-    ]
-
-    await exec.exec('zeet deploy', [
-      core.getInput('project'),
-      ...args.filter(a => a)
-    ])
-
-    core.setOutput('link', 'test')
-
-    // const status = await exec.getExecOutput(
-    //   'zeet status',
-    //   [core.getInput('project')],
-    //   {silent: true}
-    // )
-    // const links = status.stdout.match('(https?:\\/\\/zeet\\.co\\/repo[^\\s]+)')
-    // core.setOutput('link', links ? links[0] : 'Not Found')
-  } catch (e: unknown) {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    core.setFailed(e)
-  }
+  core.info(`deploy key: ${core.getInput('deploy_key')}`)
+  // core.debug(`deploy key: ${core.getInput('deploy_key')}`)
+  // core.debug(`wait: ${core.getBooleanInput('wait')}`)
+  //
+  // try {
+  //   const args = [
+  //     core.getInput('image') && `--image=${core.getInput('image')}`,
+  //     core.getInput('branch') && `--branch=${core.getInput('branch')}`,
+  //     core.getInput('follow') && `--follow=${core.getBooleanInput('wait')}`
+  //   ]
+  //
+  //   await exec.exec('zeet deploy', [
+  //     core.getInput('project'),
+  //     ...args.filter(a => a)
+  //   ])
+  //
+  //   core.setOutput('link', 'test')
+  //
+  //   // const status = await exec.getExecOutput(
+  //   //   'zeet status',
+  //   //   [core.getInput('project')],
+  //   //   {silent: true}
+  //   // )
+  //   // const links = status.stdout.match('(https?:\\/\\/zeet\\.co\\/repo[^\\s]+)')
+  //   // core.setOutput('link', links ? links[0] : 'Not Found')
+  // } catch (e: unknown) {
+  //   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  //   // @ts-ignore
+  //   core.setFailed(e)
+  // }
 }
 
 main()
